d69037401005919aa2fa37cab992f37fea1be813
Mikalai-Povarau <mikalai.povarau@thomsonreuters.com>
2023-03-01 13:46:33 +0100

    Task1: Add config for image optimization

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.module
+      .rule("images")
+      .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+      .use("image-webpack-loader")
+      .loader("image-webpack-loader")
+      .options({
+        mozjpeg: { progressive: true, quality: 65 },
+        optipng: { enabled: false },
+        pngquant: { quality: [0.4, 0.8] },
+        gifsicle: { optimizationLevel: 3 },
+        webp: { quality: 75 },
+      })
+      .end()
+  }
 });
